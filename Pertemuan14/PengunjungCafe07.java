1331cd977a9e4efbaea4c426713d381d97258de7
bdisptn <budisptn13@gmail.com>
2025-11-27 18:07:56 +0700

    Modifikasi percobaan 4

--- Pertemuan14/PengunjungCafe07.java
+++ Pertemuan14/PengunjungCafe07.java
@@ -4,13 +4,13 @@ public class PengunjungCafe07 {
     public static void daftarPengunjung(String...namaPengunjung) {
         System.out.println("Daftar nama pengunjung:");
 
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+        System.out.println("- " + nama);
         }
     }
 
     public static void main(String[] args) {
-        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung();
         daftarPengunjung("Andi");
         daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
     }
